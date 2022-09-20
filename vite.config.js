9e333e963bfc35e0650425da872292d999afaace
WaylonC <waylonwc@gmail.com>
2022-09-20 10:36:20 -0500

    meep

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+//import { sveltekit } from '@sveltejs/kit/vite';
+
+//const config = {
+//	plugins: [sveltekit()]
+//};
+
+//export default config;
+
+
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	optimizeDeps: {
+		include: ['pym.js'],
+	  },
+	plugins: [sveltekit()],
 };
 
 export default config;
