875706d7067fccd9e83e86945af1740d0704c21d
WaylonC <waylonwc@gmail.com>
2022-09-21 16:30:18 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
         console.log(index);
       }*/
 
-      current_Step.set(index);
+      index = current_Step;
 
       console.log("pymchild has received a message. the index is...");
       console.log(index);
